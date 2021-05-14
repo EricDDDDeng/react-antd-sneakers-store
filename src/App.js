b3b411afcae107a6b80a71cb21ba7846d7cdff34
ChaonengQuan <chaonengquan@email.arizona.edu>
2021-05-13 18:33:12 -0700

    add: image grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import { Layout, Menu, Input, Space } from 'antd';
 import './App.css';
+import { createUseStyles } from 'react-jss';
+import Grid from './components/Grid';
+
+const { Header, Footer, Content } = Layout;
+const { Search } = Input;
+
+//JSS
+const useStyles = createUseStyles({
+  container: {
+
+  },
+});
 
 function App() {
+  //JSS
+  const classes = useStyles();
+
+  //menu tab state
+  const [currentTab, setCurrentTab] = useState({ current: 'All' });
+  const onTabClick = e => {
+    setCurrentTab({ current: e.key });
+    alert("Current menu tab state is: " + e.key);
+  };
+
+  const onSearch = value => alert(value);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header style={{ width: '100%' }}>
+          <Menu selectedKeys={currentTab} onClick={onTabClick} mode="horizontal" theme="dark" style={{ textAlign: 'center' }}>
+            <Menu.Item key='All' >All</Menu.Item>
+            <Menu.Item key='Supreme'>Supreme</Menu.Item>
+            <Menu.Item key='Sneaker'>Sneaker</Menu.Item>
+            <Menu.Item key='Luxury'>Luxury</Menu.Item>
+          </Menu>
+        </Header>
+        <Content>
+
+          <Space direction="horizontal">
+            <Search placeholder="Find a model" allowClear onSearch={onSearch} style={{ width: 300 }} />
+            <Search placeholder="Find a size" allowClear onSearch={onSearch} style={{ width: 150 }} />
+          </Space>
+
+          <Grid></Grid>
+
+        </Content>
+        <Footer>©2021, L'inconnue</Footer>
+      </Layout>
     </div>
   );
 }
